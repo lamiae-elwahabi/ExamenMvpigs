82c2e3f3e114c7d679a0adb9f4a9dadbef0bec3a
lamiae-elwahabi <lamiaewahabi@hotmail.es>
2018-02-15 17:57:00 +0100

    funciona metodo generateKey

---
+++ lamiaeElWahabi/src/main/java/org/mvpigs/blockChain/BlockChain.java
@@ -0,0 +1,12 @@
+package org.mvpigs.blockChain;
+
+public class BlockChain {
+	//Propiedades
+	//Crear el objeto bockChain
+	BlockChain blockChain = new BlockChain();
+	
+	//Metodos 
+	public void addOrigin(){
+		
+	}
+}
---
+++ lamiaeElWahabi/src/test/java/org/mvpigs/test/WalletTest.java
@@ -0,0 +1,14 @@
+package org.mvpigs.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WalletTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
